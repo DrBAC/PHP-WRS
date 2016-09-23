3d26c24d1ec6b1bfec7b0080c31b97420a88466c
DrBAC <ben.coombs83@gmail.com>
2016-09-23 11:57:24 +0100

    23/09/16 update attempts
    trying to read out selected item(s)

--- dropdown_test_CPI.php
+++ dropdown_test_CPI.php
@@ -1,3 +1,4 @@
+
 <?php
 
 include ("login_CPI.php");
@@ -6,33 +7,66 @@ $link = mysqli_connect($hn, $un, $pw, $db);
 
 if (mysqli_connect_errno()) die(mysqli_connect_error());
 
+//$result = mysqli_query($link, "SELECT DISTINCT PROCESS_DESC FROM steps_ID");
+
 $result = mysqli_query($link, "SELECT DISTINCT PROCESS_DESC FROM steps_ID");
 
-echo "<form action=\"dropdown_test_CPI.php\" method=\"post\">";
 
-echo	"<div class=\"label\">Select Process:</div>";
-echo	"<select name=\"Process\">";
-echo	"<option value = \"\">---Select---</option>";
-while ( $d=mysqli_fetch_assoc($result)) 
+
+// ---------------------------------------------------------------------------------------------------------------- //
+
+				//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//
+				//   set up form, including drop-down box and Search Button   //
+				//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//-//
+
+echo
+
+<<<_END
+
+<form action="dropdown_test_CPI.php" method="post">
+
+<div class=\"label\">	Select Process:	</div>
+
+<select name="Process">
+
+<option value = "">		---Select---	</option>
+
+_END;
+
+while($d=mysqli_fetch_assoc($result)) 
 	{
 		$procdesc = $d['PROCESS_DESC'];
 		echo "<option value='{".$d['PROCESS_DESC']."}'>".$d['PROCESS_DESC']."</option>";
-		}
-	echo	"</select>";
+	}
+
+echo
+
+<<<_END
+
+</select>
 	
-echo "<input type=\"submit\" value=Search>";
-echo "</form>";
+<input type="submit" value=Search>
 
+</form>
 
-// ------------------------------------------------------------------------------------------------------------------------------
+_END;
+
+// ---------------------------------------------------------------------------------------------------------------- //
 
 if (isset($_POST['Process']))			//sees if form was used (selection - press search)
 {
-	$choice = $_POST['Process'];
-	$response = mysqli_query($link, "SELECT * FROM steps_ID WHERE PROCESS_DESC='$choice'");
-	$rows = $response->num_rows;
+	$choice = trim(strip_tags($_POST['Process']));
+	
+	$response = mysqli_query($link, "SELECT * FROM steps_ID WHERE PROCESS_DESC = '$choice'");
+	
+//	$rows = $response->num_rows;
+
+// create table header
 	 
-	echo "<br>
+echo
+<<<_TABHEAD
+
+	<br>
 	<table>
 	<tr>
 	<th>Step ID</th>
@@ -40,20 +74,25 @@ if (isset($_POST['Process']))			//sees if form was used (selection - press searc
 	<th>Tool</th>
 	<th>Recipe</th>
 	<th>Details</th>
-	</tr>"; //HEADER OF TABLE
+	</tr>
+
+_TABHEAD;
  
-	for ($j = 0 ; $j < $rows ; ++$j)
-	{
-		$response->data_seek($j);
-		$row = $response->fetch_array(MYSQLI_ASSOC);
-	
+//	for ($j = 0 ; $j < $rows ; ++$j)
+
+//		$response->data_seek($j);
+
+//		$row = $response->fetch_array(MYSQLI_ASSOC);
+
+while($row = mysqli_fetch_array($response, MYSQLI_BOTH))
+{
 	echo "<tr>";
-	echo "<td>$row[STEP_ID]</td>
-	<td>$row[PROCESS_DESC]</td>
-	<td>$row[TOOL]</td>
-	<td>$row[RECIPE]</td>
-	<td>$row[DETAILS]</td>
-	<td></form></td>";
+	echo "<td>" . $row['STEP_ID'] . "</td>";
+	echo "<td>" . $row['PROCESS_DESC'] . "</td>";
+	echo "<td>" . $row['TOOL'] . "</td>";
+	echo "<td>" . $row['RECIPE'] . "</td>";
+	echo "<td>" . $row['DETAILS'] . "</td>";
+	echo "<td></form></td>";
   	
 	echo "</tr>";
 	}
@@ -62,7 +101,7 @@ if (isset($_POST['Process']))			//sees if form was used (selection - press searc
 //	mysqli_free_result($response);
 	
 	 echo "$choice";
-	 
+	//echo $response;
  }
 
 ?>
