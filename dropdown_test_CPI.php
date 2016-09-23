1ddb1f3a3914f8a169ec585f36611fc6ac65daf4
DrBAC <ben.coombs83@gmail.com>
2016-09-23 10:03:01 +0100

    style and formatting changes
    still not kicking out the data!!!

--- dropdown_test_CPI.php
+++ dropdown_test_CPI.php
@@ -6,7 +6,7 @@ $link = mysqli_connect($hn, $un, $pw, $db);
 
 if (mysqli_connect_errno()) die(mysqli_connect_error());
 
-$result = mysqli_query($link, "SELECT DISTINCT`PROCESS_DESC` FROM `steps_ID`");
+$result = mysqli_query($link, "SELECT DISTINCT PROCESS_DESC FROM steps_ID");
 
 echo "<form action=\"dropdown_test_CPI.php\" method=\"post\">";
 
@@ -21,30 +21,47 @@ while ( $d=mysqli_fetch_assoc($result))
 	echo	"</select>";
 	
 echo "<input type=\"submit\" value=Search>";
-
 echo "</form>";
 
- if (isset($_POST['Process']))
- {
-	 $choice = $_POST['Process'];
-	 $sql = "SELECT * FROM steps_ID WHERE PROCESS_DESC='$choice'";
-	 $response = mysqli_query($link, $sql);
 
+// ------------------------------------------------------------------------------------------------------------------------------
 
-  echo "<br><table><tr><th>Step ID</th><th>Process Description</th><th>Tool</th><th>Recipe</th><th>Details</th></tr>"; //HEAD OF TABLE
-
- while($row = mysqli_fetch_array($response, MYSQLI_ASSOC))
-  {
-  	echo "<tr>";
-
-	echo "<td>$row[STEP_ID]</td><td>$row[PROCESS_DESC]</td><td>$row[TOOL]</td><td>$row[RECIPE]</td><td>$row[DETAILS]</td>
+if (isset($_POST['Process']))			//sees if form was used (selection - press search)
+{
+	$choice = $_POST['Process'];
+	$response = mysqli_query($link, "SELECT * FROM steps_ID WHERE PROCESS_DESC='$choice'");
+	$rows = $response->num_rows;
+	 
+	echo "<br>
+	<table>
+	<tr>
+	<th>Step ID</th>
+	<th>Process Description</th>
+	<th>Tool</th>
+	<th>Recipe</th>
+	<th>Details</th>
+	</tr>"; //HEADER OF TABLE
+ 
+	for ($j = 0 ; $j < $rows ; ++$j)
+	{
+		$response->data_seek($j);
+		$row = $response->fetch_array(MYSQLI_ASSOC);
+	
+	echo "<tr>";
+	echo "<td>$row[STEP_ID]</td>
+	<td>$row[PROCESS_DESC]</td>
+	<td>$row[TOOL]</td>
+	<td>$row[RECIPE]</td>
+	<td>$row[DETAILS]</td>
 	<td></form></td>";
   	
 	echo "</tr>";
-  }
-  echo "</table>";
-	mysqli_free_result($response);
+	}
+	echo "</table>";
 	
+//	mysqli_free_result($response);
+	
+	 echo "$choice";
 	 
  }
 
