b11fd1250c0add11a81f866d1855530e874dba80
DrBAC <ben.coombs83@gmail.com>
2016-10-10 11:43:55 +0100

    Attempts at dropdown with php

--- Citys_Nathan.php
+++ Citys_Nathan.php
@@ -12,48 +12,52 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title> Dropdown AJAX </title>
+	<title> Dropdown auto complete </title>
 </head>
-
-<style type="text/css">
-	.country, .city{
-		margin : 20px;
-		text-align: centre;
-	}
-	
-</style>
 
 <body>
 
-<div class = "country">
+<p> Select a country: </p>
+
+<div class = "country_selection">
 
 	<label>Country</label>
-	<select name = "country" onchange="getId(this.value);">
+	<select name = "country_select" onchange="myFunction()"> 
 		<option value = "">Select Country</option>
-		
-		<!-- populate values using php -->
 		<?php
 			$query = "SELECT * FROM country";
 			$result = mysqli_query($con, $query);
 			// loop
 			foreach ($result as $country) {
 			?>
-		<option value = "<?php echo $country["cid"]; ?>"><?php echo $country["country"]; ?></option>
+		<option value = "<?php echo $country["country"]; ?>"><?php echo $country["country"]; ?></option>
 		<?php
-			
-				}
+				} //closes loop for first select box
 			?>
 	</select>
 </div>
 
+<p>When you select a new coutry, a function is triggered which outputs the value of the selected country.</p>
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+    var x = document.getElementById("country_select").value;
+    document.getElementById("demo").innerHTML = "You selected: " + x;
+}
+</script>
+
+
+<!--
 
 <div class = "city">
 	<label>City</label>
 	<select name = "city" id="cityList">
-		<option value = ""></option>
+		<option value = ""> "cid"</option>
 	
 	</select>
 </div>
+
 
 <script   src="https://code.jquery.com/jquery-3.1.1.js"   integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA="   crossorigin="anonymous"></script>
 <script>
@@ -71,7 +75,7 @@
 		}
 		
 </script>
-
+-->
 
 
 </body>
