98368d9c2bb9fcc432fa3f8560f4ede2fb72f7c5
DrBAC <ben.coombs83@gmail.com>
2016-10-10 14:18:10 +0100

    got auto return working
    changed select name to select ID

--- Citys_Nathan.php
+++ Citys_Nathan.php
@@ -14,15 +14,14 @@
 <head>
 	<title> Dropdown auto complete </title>
 </head>
-
 <body>
+
 
 <p> Select a country: </p>
 
 <div class = "country_selection">
-
 	<label>Country</label>
-	<select name = "country_select" onchange="myFunction()"> 
+	<select id = "country_select" onchange="myFunction()"> 
 		<option value = "">Select Country</option>
 		<?php
 			$query = "SELECT * FROM country";
@@ -37,8 +36,14 @@
 	</select>
 </div>
 
-<p>When you select a new coutry, a function is triggered which outputs the value of the selected country.</p>
+
+
+
+<p>When you select a new country, a function is triggered which outputs the value of the selected country.</p>
 <p id="demo"></p>
+
+
+
 
 <script>
 function myFunction() {
@@ -48,34 +53,17 @@ function myFunction() {
 </script>
 
 
-<!--
 
-<div class = "city">
+
+<div class = "city_selection">
 	<label>City</label>
 	<select name = "city" id="cityList">
-		<option value = ""> "cid"</option>
+		<option value = ""> x </option>
 	
 	</select>
-</div>
 
+	</div>
 
-<script   src="https://code.jquery.com/jquery-3.1.1.js"   integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA="   crossorigin="anonymous"></script>
-<script>
-		function getId(val){
-			alert(val);
-//			$.ajax({
-//				type: "POST",
-//				url: "getdata.php"
-//				data: "cid="+val,
-//				success: function(data){
-//					$("#cityList").html(data);
-				}
-	//		});
-			
-		}
-		
-</script>
--->
 
 
 </body>
